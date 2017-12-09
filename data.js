8e988c6d0789051f3ac5be3402813a6fdd52ff5f
lukelee365 <lukelee365@gmail.com>
2017-12-09 14:53:43 -0500

    new Update

---
+++ data.js
@@ -0,0 +1,510 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-point-m",
+      "name": "Point M",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": -0.7627676465694222,
+        "pitch": 0.18260814786458823,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5757379978301849,
+          "pitch": 0.37131172431092097,
+          "rotation": 0,
+          "target": "1-point-a"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0743378956495242,
+          "pitch": 0.1252041703962785,
+          "title": "Living Room",
+          "text": "Some Descriptions"
+        }
+      ]
+    },
+    {
+      "id": "1-point-a",
+      "name": "Point A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7496234362903103,
+          "pitch": 0.3057060565815206,
+          "rotation": 0,
+          "target": "0-point-m"
+        },
+        {
+          "yaw": 0.03319895399098982,
+          "pitch": 0.33875922371870715,
+          "rotation": 0,
+          "target": "3-point-e"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-point-l",
+      "name": "Point L",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": -2.929920683070165,
+        "pitch": -0.0017709517657564788,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3494636832821101,
+          "pitch": 0.6758715875972232,
+          "rotation": 0,
+          "target": "6-point-h"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5708877871589717,
+          "pitch": -0.43270819929633575,
+          "title": "Skype Superman",
+          "text": "BlaBlaBla"
+        },
+        {
+          "yaw": 2.5765800563665167,
+          "pitch": -0.25880972844589323,
+          "title": "Washroom",
+          "text": "Descriptions Washroom"
+        }
+      ]
+    },
+    {
+      "id": "3-point-e",
+      "name": "Point E",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": 0.7717500788981564,
+        "pitch": 0.10799612382974821,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.378353944377153,
+          "pitch": 0.29487909255155564,
+          "rotation": 0,
+          "target": "1-point-a"
+        },
+        {
+          "yaw": 0.18059527093711125,
+          "pitch": 0.4001831537345346,
+          "rotation": 0,
+          "target": "4-point-f"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-point-f",
+      "name": "Point F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": 2.7153804952137293,
+        "pitch": 0.2156355990246741,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07664253289080314,
+          "pitch": 0.6742353134355223,
+          "rotation": 0,
+          "target": "3-point-e"
+        },
+        {
+          "yaw": -3.105792050036664,
+          "pitch": 0.717359130194108,
+          "rotation": 0,
+          "target": "5-point-g"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-point-g",
+      "name": "Point G",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": -0.020163194870310264,
+        "pitch": 0.31259963295242343,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.552110625261328,
+          "pitch": 0.5525322135657209,
+          "rotation": 0,
+          "target": "4-point-f"
+        },
+        {
+          "yaw": -0.02016836885072948,
+          "pitch": 0.7634273838827799,
+          "rotation": 0,
+          "target": "7-point-i"
+        },
+        {
+          "yaw": 1.5868780018576398,
+          "pitch": 0.7393013710995291,
+          "rotation": 0,
+          "target": "6-point-h"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-point-h",
+      "name": "Point H",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": 1.7680119718023786,
+        "pitch": 0.3009831473031035,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5198328599464048,
+          "pitch": 0.8038388471234565,
+          "rotation": 0,
+          "target": "5-point-g"
+        },
+        {
+          "yaw": -1.5011917862053465,
+          "pitch": 0.28637052165567667,
+          "rotation": 0,
+          "target": "3-point-e"
+        },
+        {
+          "yaw": 0.1880490154991712,
+          "pitch": 0.6559162391850037,
+          "rotation": 0,
+          "target": "8-point-j"
+        },
+        {
+          "yaw": -2.9934243852214255,
+          "pitch": 0.7108913709967233,
+          "rotation": 0,
+          "target": "2-point-l"
+        },
+        {
+          "yaw": 1.8801379772408566,
+          "pitch": 0.7183178557533108,
+          "rotation": 0,
+          "target": "9-point-k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-point-i",
+      "name": "Point I",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": 0.23362918009749833,
+        "pitch": 0.18285085886941843,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.05237483666572,
+          "pitch": 0.44294246583018193,
+          "rotation": 0,
+          "target": "5-point-g"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5726711713559247,
+          "pitch": -0.3783790346766427,
+          "title": "Owner Skype",
+          "text": "Philips"
+        },
+        {
+          "yaw": -0.3300535148178767,
+          "pitch": 0.21944584348861085,
+          "title": "Room1",
+          "text": "Descriptions"
+        }
+      ]
+    },
+    {
+      "id": "8-point-j",
+      "name": "Point J",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.081371658597652,
+          "pitch": 0.6197506023761363,
+          "rotation": 0,
+          "target": "6-point-h"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.48653812980344213,
+          "pitch": -0.4766306942071843,
+          "title": "Skype RadioShack",
+          "text": "BlaBlaBla"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "Room2",
+          "text": "Descriptions 2"
+        }
+      ]
+    },
+    {
+      "id": "9-point-k",
+      "name": "Point K",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": 0.34277390335079083,
+        "pitch": -0.03659561334826833,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8393162157635956,
+          "pitch": 0.7187625041226848,
+          "rotation": 0,
+          "target": "6-point-h"
+        },
+        {
+          "yaw": 1.7118213981263066,
+          "pitch": 0.3513382024125953,
+          "rotation": 0,
+          "target": "5-point-g"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5534440498734483,
+          "pitch": -0.34041367007585066,
+          "title": "Skype Alex",
+          "text": "BlaBlaBla"
+        },
+        {
+          "yaw": -0.19119447113019383,
+          "pitch": 0.034787047625979284,
+          "title": "Room3",
+          "text": "Description 3"
+        }
+      ]
+    }
+  ],
+  "name": "Demo",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
